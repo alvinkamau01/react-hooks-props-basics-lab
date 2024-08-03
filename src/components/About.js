675c8d6e98f4b9c6e0567c8a04365e5280dad3c6
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-08-03 11:16:51 +0300

    Assignments

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ about={} }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      
+      {about.bio && <p>{about.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <div><Links links={about.links} /></div>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+function Links({links}){
+    return(
+        <div id="links">
+            <h3>Links</h3>
+        <p><a href={links.github}>"https://github.com/liza"</a></p>
+        <p><a href={links.linkedin}></a>"https://www.linkedin.com/in/liza/"</p>
+        </div>
+        
+    )
+}
+export default Links
